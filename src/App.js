9271ff1f45780f005e08df561f10edd2d9e0c90d
dmojarrot <a01638460@itesm.mx>
2022-11-15 13:15:10 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react"
-import { fillData, shuffle } from "./functions/fillData.jsx"
+import { fillData } from "./functions/fillData.jsx"
 import toast from "react-hot-toast"
 
 const selection = []
-const indexes = []
 
 function App() {
   const [data, setData] = useState([])
+  const [totalCards, setTotalCards] = useState(81)
   useEffect(() => {
     if (data.length === 0) {
       setData(fillData())
@@ -30,7 +30,6 @@ function App() {
       figure = 3
     }
 
-    indexes.push([index])
     selection.push([figure, item.color, item.num, shading, index])
 
     if (selection.length === 3) {
@@ -51,13 +50,11 @@ function App() {
         (selection[0][3] === selection[2][3]) +
         (selection[1][3] === selection[2][3])
 
-      console.log(isShape, isColor, isSymbol, isShade)
-
       if (isShape === 0 || isShape === 3) {
         if (isColor === 0 || isColor === 3) {
           if (isSymbol === 0 || isSymbol === 3) {
             if (isShade === 0 || isShade === 3) {
-              console.log("in")
+              toast.success("Correct set!")
               const newData = data.filter(
                 (_item, index) =>
                   index !== selection[0][4] &&
@@ -65,56 +62,82 @@ function App() {
                   index !== selection[2][4]
               )
               setData(newData)
-              toast.success("Correct set!")
+              setTotalCards((prev) => prev - 3)
             } else {
               toast.error("Incorrect set!")
+              setData((prev) => {
+                let newData = [...prev]
+                newData[selection[0][4]].selected = false
+                newData[selection[1][4]].selected = false
+                newData[selection[2][4]].selected = false
+                return newData
+              })
             }
           } else {
             toast.error("Incorrect set!")
+            setData((prev) => {
+              let newData = [...prev]
+              newData[selection[0][4]].selected = false
+              newData[selection[1][4]].selected = false
+              newData[selection[2][4]].selected = false
+              return newData
+            })
           }
         } else {
           toast.error("Incorrect set!")
+          setData((prev) => {
+            let newData = [...prev]
+            newData[selection[0][4]].selected = false
+            newData[selection[1][4]].selected = false
+            newData[selection[2][4]].selected = false
+            return newData
+          })
         }
       } else {
         toast.error("Incorrect set!")
+        setData((prev) => {
+          let newData = [...prev]
+          newData[selection[0][4]].selected = false
+          newData[selection[1][4]].selected = false
+          newData[selection[2][4]].selected = false
+          return newData
+        })
       }
-      setData((prev) => {
-        let newData = [...prev]
-        newData[indexes[0]].selected = false
-        newData[indexes[1]].selected = false
-        newData[indexes[2]].selected = false
-        return newData
-      })
-
+    }
+    if (selection.length === 4) {
       selection.splice(0, 3)
     }
   }
-  console.log(indexes)
 
   return (
     <div className="bg-slate-800 w-screen h-screen">
-      <div className="grid grid-cols-3 w-10/12 md:w-7/12 mx-auto pt-20 gap-5">
-        {data.slice(0, 12).map((item, index) => (
-          <button
-            key={index}
-            onClick={() => selector(item, index)}
-            className="hover:bg-slate-700 disabled:bg-slate-700 rounded-md border border-slate-700"
-            disabled={item.selected}
-          >
-            <div className="flex gap-2 p-5 justify-center ">
-              {[...Array(item.num)].map((_val, i) => (
-                <img
-                  key={i}
-                  className={`${item.color}`}
-                  src={item.href.url}
-                  alt="W3Schools.com"
-                  width={30}
-                  height={30}
-                />
-              ))}
-            </div>
-          </button>
-        ))}
+      <div className="w-10/12 md:w-7/12 mx-auto">
+        <div className="grid grid-cols-3 pt-20 gap-5">
+          {data.slice(0, 12).map((item, index) => (
+            <button
+              key={index}
+              onClick={() => selector(item, index)}
+              className="hover:bg-slate-700 disabled:bg-slate-700 rounded-md border border-slate-700"
+              disabled={item.selected}
+            >
+              <div className="flex gap-2 p-5 justify-center ">
+                {[...Array(item.num)].map((_val, i) => (
+                  <img
+                    key={i}
+                    className={`${item.color}`}
+                    src={item.href.url}
+                    alt="W3Schools.com"
+                    width={30}
+                    height={30}
+                  />
+                ))}
+              </div>
+            </button>
+          ))}
+        </div>
+        <h1 className="text-slate-400 text-lg mt-5">
+          Total cards left: {totalCards}
+        </h1>
       </div>
     </div>
   )
